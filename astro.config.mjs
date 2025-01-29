5a2fbce3999601f8973d512e0933e5b91cb3e799
Jason Lengstorf <jason@lengstorf.com>
2025-01-29 15:06:58 -0800

    feat: branding, styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://v2.learnwithjason.dev',
+	site: 'https://codetv.dev',
 	output: 'server',
 	integrations: [
 		clerk({
