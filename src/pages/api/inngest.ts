acce6fe1ec92d2525d78ce9213a2b11f8f345032
Jason Lengstorf <jason@lengstorf.com>
2025-02-02 19:04:40 -0800

    feat: inngest

--- src/pages/api/inngest.ts
+++ src/pages/api/inngest.ts
@@ -1,8 +1,6 @@
 import { serve } from 'inngest/astro';
 import { functions, inngest } from '../../util/inngest';
 
-console.log('hello?');
-
 export const { GET, POST, PUT } = serve({
 	client: inngest,
 	functions,
